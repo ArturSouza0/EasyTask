b6013e8eaed71c6224d2bea8f0a80beb2611a551
ArturSouza0 <artur.souza955@gmail.com>
2025-08-14 23:51:50 -0300

    refactor: remove standalone components and organize module imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent,UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
